440a5ab8495f7b5d66e62f1745d21ade96a10b22
Kyle Kwong <kyle@lob.com>
2016-05-27 11:17:39 -0700

    feat(movies): adds functionality for adding movies to database

---
+++ test/plugins/features/movies/controller.test.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const Controller = require('../../../../lib/plugins/features/movies/controller');
+const Movie      = require('../../../../lib/models/movie');
+
+describe('movie controller', () => {
+
+  describe('create', () => {
+
+    it('creates a movie', () => {
+      const payload = {
+        title: 'Inception',
+        release_year: 2010
+      };
+
+      return Controller.create(payload)
+      .then((movie) => {
+        expect(movie.get('title')).to.eql(payload.title);
+        expect(movie.get('release_year')).to.eql(payload.release_year);
+
+        return new Movie({ id: movie.id }).fetch();
+      })
+      .then((movie) => {
+        expect(movie.get('title')).to.eql(payload.title);
+        expect(movie.get('release_year')).to.eql(payload.release_year);
+      });
+    });
+
+  });
+
+});
